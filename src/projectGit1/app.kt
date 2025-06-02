68f98b837ea2ed814db3a3f50c9e15d8223ed67e
Rodion0799 <rzaevrodion99@gmail.com>
2025-06-02 20:11:15 +0300

    Добавление методов + комментарии

--- src/projectGit1/app.kt
+++ src/projectGit1/app.kt
@@ -2,18 +2,14 @@ package projectGit1
 
 //start
 fun main() {
+    val mapFunPassword = mutableMapOf<String, String>()
     println("Вас приветствует OC")
-    val command = readLine()!!
+    val userStart = readLine()!!
 
-    if (command.isBlank()) {
-        println("Пустой ввод - Ошибка! ")
-        return
+    if (userStart.isBlank() || userStart != "Команды") {
+        throw IllegalArgumentException("Ошибка! ")
     }
     while (true) {
-        if (command != "Команды") {
-            throw IllegalArgumentException("Такой команды нет!")
-        }
-
         println("Выберите -> Пароль / Try-Catch / Cat or Dog / Калькулятор / Sort / Bank / Конец ")
         val userVs = readLine()!!
         if (userVs.isBlank()) {
@@ -24,61 +20,15 @@ fun main() {
             "Bank" -> if (bankTerminal()) return
             "Sort" -> sort()
             "Калькулятор" -> if (calculator()) break
-            "Пароль" -> {
-                print("Имя пользователя: ")
-                val userName = readLine()!!
-                if (userName.isBlank()) {
-                    println("Неккоректное значение! ")
-                    return
-                }
-
-                println("Придумайте пароль! ")
-                val user = readLine()!!
-                try {
-                    if (user.length < 5) {
-                        throw PasswordException("Пароль меньше 5 символов! ")
-                    } else {
-                        println("Ваш пароль -> $user ")
-                    }
-                } catch (e: PasswordException) {
-                    println(e.message)
-                }
-
-                println("История паролей; ")
-                val mapPassword: MutableMap<String, String> = mutableMapOf(userName to user)
-                println(mapPassword)
-            }
-
-            "Try-Catch" -> {
-                print("Введите число: ")
-                val userTry = readLine()!!
-                try {
-                    val toIntVal = userTry.toInt()
-                    println("Ваше число -> $toIntVal")
-                } catch (e: NumberFormatException) {
-                    println(("$userTry - Это не число!"))
-                }
-            }
-
-            "Cat or Dog" -> {
-                val dog: Box<Dog> = Box(Dog())
-                val animal: Box<Animal> = dog
-                println(animal.getItem())
-
-                val animal2: Feeder<Animal> = Feeder()
-                val cat: Feeder<Cat> = animal2
-                cat.feed(Cat())
-            }
-
+            "Пароль" -> if (password(mapFunPassword)) return
+            "Try-Catch" -> tryCatch()
+            "Cat or Dog" -> carOrDog()
             "Конец" -> {
-                println("Конец!")
+                finish()
                 break
             }
 
-            else -> {
-                throw IllegalArgumentException("Введите правильную команду!")
-            }
+            else -> throw IllegalArgumentException("Введите правильную команду!")
         }
-
     }
 }
--- src/projectGit1/services.kt
+++ src/projectGit1/services.kt
@@ -1,6 +1,9 @@
 package projectGit1
 
-
+/**
+ * Демонстрация простого калькулятора
+ * с обработкой ввода и базовых арифметических операций.
+ */
 fun calculator(): Boolean {
     print("Введите первое число -> ")
     val a = readLine()!!.toInt()
@@ -33,24 +36,25 @@ fun calculator(): Boolean {
 /**
  * sort
  */
- fun sort() {
+fun sort() {
     print("Введите числа для сортировки -> ")
     val userSort = readLine() ?: ""
 
     try {
-        val numbers = userSort.split(" ")
+        val numbers = userSort.split(", ")
             .map { it.toInt() }
             .sorted()
         println(numbers)
     } catch (e: NumberFormatException) {
         println("Вы ввели не число! ")
+    } catch (e: IllegalArgumentException) {
+        println("Введите числа через запятую! ")
     }
 }
 
 
 /**
  * Демонстрация работ банковского терминала
- *
  * @return Boolean -> true (если баланса хватает) false (если не хватает)
  */
 fun bankTerminal(): Boolean {
@@ -81,4 +85,76 @@ fun bankTerminal(): Boolean {
         println("Баланс $userName -> ${userBank1.balance}\nБаланс банка -> ${bank.balance}")
     }
     return false
+}
+
+
+/**
+ * Демонстрация валидации ввода при регистрации:
+ * проверка логина и пароля,
+ * использование MutableMap,
+ * обработка исключений, вывод данных.
+ */
+fun password(map: MutableMap<String, String>): Boolean {
+    print("Имя пользователя: ")
+    val userName = readLine()!!
+    if (userName.isBlank()) {
+        println("Неккоректное значение! ")
+        return true
+    }
+
+    print("Придумайте пароль: ")
+    val userPassword = readLine()!!
+    try {
+        if (userPassword.length < 5) {
+            throw PasswordException("Пароль меньше 5 символов! ")
+        } else {
+            map.put(userName, userPassword)
+        }
+    } catch (e: PasswordException) {
+        println(e.message)
+    }
+    map.forEach { name, password ->
+        println("$name: $password")
+    }
+    return false
+}
+
+
+/**
+ * Демонстрация обработки исключений
+ * при преобразовании строки в число с использованием try-catch.**
+ */
+fun tryCatch() {
+    print("Введите число: ")
+    val userString = readLine()!!
+    if (userString.isBlank()) {
+        println("Пустой ввод!")
+        return
+    }
+    try {
+        val userInt = userString.toInt()
+        println("Ваше число -> $userInt")
+    } catch (e: NumberFormatException) {
+        println("$userString - Это не число!")
+    }
+}
+
+/**
+ * Демонстрация ковариантности и контрвариантности с использованием in и out.
+ */
+fun carOrDog() {
+    val dog: Box<Dog> = Box(Dog())
+    val animal: Box<Animal> = dog
+    println(animal.getItem())
+
+    val animal2: Feeder<Animal> = Feeder()
+    val cat: Feeder<Cat> = animal2
+    cat.feed(Cat())
+}
+
+/**
+ * Finish
+ */
+fun finish() {
+    println("До скорых встреч!")
 }
